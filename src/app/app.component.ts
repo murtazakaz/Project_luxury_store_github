b286fa2b267f7479232435729f0df3e1b1261193
Syed Murtaza <syed.murtaza@rocketmail.com>
2017-09-29 16:45:29 +0500

    29 sept initial push

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform ,ModalController} from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { SplashPage } from '../pages/splash/splash';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = LoginPage;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen,public modalCtrl: ModalController) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'Logout' , component: LoginPage }
+    ];
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      let splash = modalCtrl.create(SplashPage);
+      splash.present();
+      // this.splashScreen.hide();
+    });
+  }
+
+  initializeApp() {
+   
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ src/pages/login/login.ts
@@ -0,0 +1,34 @@
+import { HomePage } from '../home/home';
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+  loginpage()
+  {
+    this.navCtrl.push(HomePage);
+  }
+  registerpage()
+  {
+    this.navCtrl.push(RegisterPage);
+  }
+}
