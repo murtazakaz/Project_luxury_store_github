d60f4a1c28dbd6d6a5ea3252a4d9ef5d7b03a8f4
Syed Murtaza <syed.murtaza@rocketmail.com>
2017-10-03 19:00:39 +0500

    3 oct 2017

--- src/pages/store-itemlist/store-itemlist.ts
+++ src/pages/store-itemlist/store-itemlist.ts
@@ -5,6 +5,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import {Observable} from "rxjs/Observable";
 import { StoreItemdetailsPage } from '../store-itemdetails/store-itemdetails';
+import { LoadingController } from 'ionic-angular';
 /**
  * Generated class for the StoreItemlistPage page.
  *
@@ -20,12 +21,18 @@ export class StoreItemlistPage {
  public itemarray:any;
  public itemcategory:any;
  public itemid:any;
-  constructor(public navCtrl: NavController, public navParams: NavParams,private http: Http) {
+  constructor(public navCtrl: NavController, public navParams: NavParams,private http: Http,public loadingCtrl: LoadingController) {
    
     this.itemcategory = this.navParams.get('cat');
    
+      const loading = this.loadingCtrl.create({
+        content: 'Please wait...'
+      });
+      loading.present();
+      
   this.http.get('http://govirtualstore.com/app/iOS/fetchcategories.php?type='+this.itemcategory).map(res => res.json()).subscribe(data => {
     console.log(data);
+    loading.dismiss();
   this.itemarray = data;
   // var i;
   // for(i in this.itemarray)
